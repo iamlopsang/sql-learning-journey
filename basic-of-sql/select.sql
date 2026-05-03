7cdfdbc459cf3a8da72632e4763e8265828b7da1
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-03 22:29:22 +0545

    update

---
+++ basic-of-sql/select.sql
@@ -0,0 +1,25 @@
+##  PIXAR MOVIE QUERIES: Exercise 1
+        = This script solves basic SELECT queries for the movies table.
+
+## 1. Find the title of each film.
+
+        SELECT title FROM movies;
+
+## 2. Find the director of each film.
+
+        SELECT director FROM movies;
+
+
+## 3. Find the title and director of each film.
+
+        SELECT title, director FROM movies;
+
+
+## 4. Find the title and year of each film.
+
+        SELECT title, year FROM movies;
+
+## 5. Find all the information about each film.
+
+        SELECT * FROM movies;
+
