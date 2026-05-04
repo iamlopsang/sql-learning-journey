8db330377b96b6acc137fa1a1492fd0a0b25c36f
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-04 23:06:25 +0545

    update

--- basic-of-sql/select.sql
+++ basic-of-sql/select.sql
@@ -5,6 +5,8 @@
 
         SELECT title FROM movies;
 
+        -- Explanation: Selects only the 'title' column from the 'movies' table.
+
 ## 2. Find the director of each film.
 
         SELECT director FROM movies;
@@ -13,13 +15,15 @@
 ## 3. Find the title and director of each film.
 
         SELECT title, director FROM movies;
+        -- Explanation: Selects both the 'title' and 'director' columns from the 'movies' table.
 
 
 ## 4. Find the title and year of each film.
 
         SELECT title, year FROM movies;
+        -- Explanation: Selects both the 'title' and 'year' columns from the 'movies' table.
 
 ## 5. Find all the information about each film.
 
         SELECT * FROM movies;
-
+        -- Explanation: Selects all columns from the 'movies' table using the '*' which is used to select all columns.
