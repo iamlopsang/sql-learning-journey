f4ad3f0cf0e05a441a66fe07c4e55f80305a80b0
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-05 21:02:36 +0545

    where condition

---
+++ where-condition-sql/where_condition.sql
@@ -0,0 +1,6 @@
+
+Exercise 2 — Tasks
+Find the movie with a row id of 6
+
+SELECT * FROM movies WHERE rowid = 6;
+-- Explanation: This query selects all columns from the 'movies' table where the 'rowid' is equal to 6. The 'WHERE' clause is used to filter the results based on a specified condition.
