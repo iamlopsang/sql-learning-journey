52dae934067897bb1e290ef424f289ae00bbed44
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-14 21:14:19 +0545

    add SQL queries to list directors and last four Pixar movies

--- filtering -and-sorting-sql/filtering-and-sorting.sql
+++ filtering -and-sorting-sql/filtering-and-sorting.sql
@@ -4,3 +4,10 @@ SELECT DISTINCT director
 FROM movies
 WHERE studio = 'Pixar'
 ORDER BY director ASC;
+
+List the last four Pixar movies released (ordered from most recent to least)
+
+SELECT title, release_date
+FROM movies WHERE studio = 'Pixar'
+ORDER BY release_date DESC  
+LIMIT 4;
