35a9abfb2b48f882b619f3ba8015d655f2a72002
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-15 21:48:51 +0545

    add SQL queries to list first five Pixar movies and those released in the 1990s

--- filtering -and-sorting-sql/filtering-and-sorting.sql
+++ filtering -and-sorting-sql/filtering-and-sorting.sql
@@ -11,3 +11,19 @@ SELECT title, release_date
 FROM movies WHERE studio = 'Pixar'
 ORDER BY release_date DESC  
 LIMIT 4;
+
+List the first five Pixar movies sorted alphabetically.
+
+SELECT title
+FROM movies WHERE studio = 'Pixar'
+ORDER BY title ASC
+LIMIT 5;
+
+List all Pixar movies released in the 1990s, sorted by release date (oldest to newest)  
+
+SELECT title, release_date
+FROM movies                 
+WHERE studio = 'Pixar' AND release_date >= '1990-01-01' AND release_date < '2000-01-01'
+ORDER BY release_date ASC;
+
+
