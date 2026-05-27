9b304b2f0f3f7d2d4576147a084cca3efb61a44e
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-05-27 21:45:57 +0545

    fix: correct SQL queries for Canadian cities and US cities by latitude

--- Exercise-of-sql/exercise1.sql
+++ Exercise-of-sql/exercise1.sql
@@ -31,7 +31,17 @@
 
 ##  Questions:
     1.List all the Canadian cities and their populations.
+    select city, population
+    from cities
+    where country="Canada";
     2.Order all the cities in the United States by their latitude from north to south.
+    selet city,latitude
+    from cities
+    where country="United States"
+    order by latitude desc;
     3.List all the cities west of Chicago, ordered from west to east.
+    select city, longitude
+    from cities
+    where city="Chicago" order by longitude desc;
     4.List the two largest cities in Mexico (by population).
     5.List the third and fourth largest cities (by population) in the United States and their population.
