0fdf5c01f7508d3df725e4730fb134a99eb9e9e3
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-07-23 08:42:56 +0545

    solving questions of basic of sql

--- basic-of-sql/select.sql
+++ basic-of-sql/select.sql
@@ -1,4 +1,5 @@
--- ##  PIXAR MOVIE QUERIES: Exercise 1
+                -- ##  PIXAR MOVIE QUERIES: Exercise 1
+                
 --         = This script solves basic SELECT queries for the movies table.
 
 -- ## 1. Find the title of each film.
@@ -11,19 +12,24 @@
 
         SELECT director FROM movies;
 
+-- Explanation: This query displays the names of all directors from the 'movies' table.
 
 -- ## 3. Find the title and director of each film.
 
         SELECT title, director FROM movies;
+
         -- Explanation: Selects both the 'title' and 'director' columns from the 'movies' table.
 
 
 -- ## 4. Find the title and year of each film.
 
         SELECT title, year FROM movies;
+
         -- Explanation: Selects both the 'title' and 'year' columns from the 'movies' table.
 
 -- ## 5. Find all the information about each film.
 
         SELECT * FROM movies;
-        -- Explanation: Selects all columns from the 'movies' table using the '*' which is used to select all columns.
+
+        -- Explanation: Selects all columns from the 'movies' table using the '*' which is used to select
+        --  all columns.
