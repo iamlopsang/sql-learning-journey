3c58a028698d9d59494a62aba59bfbe026e97ea3
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-08-10 22:04:59 +0545

    refactor: update SQL JOIN exercise tasks for clarity and formatting

--- SQL-JOIN/exercise-of-sql-join.sql
+++ SQL-JOIN/exercise-of-sql-join.sql
@@ -1,5 +1,5 @@
+    -- ##  SQL JOIN Exercise: Movie Sales Analysis (INNER JOIN)
 
-        Exercise 6 — Tasks
-        Find the domestic and international sales for each movie
-        Show the sales numbers for each movie that did better internationally rather than domestically
-        List all the movies by their ratings in descending order
+    -- a. Find the domestic and international sales for each movie.
+    -- b. Show the sales numbers for each movie that did better internationally rather than domestically.
+    -- c. List all the movies by their ratings in descending order.
