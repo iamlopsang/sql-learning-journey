d5db5cc1283f940acf5a25638ed9e73565281c8f
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-08-26 08:55:03 +0545

    feat: add SQL questions for multiple tables practice

---
+++ join-with-multiple-tables/multiple_tables.sql
@@ -0,0 +1,68 @@
+-- Level 1
+-- Question 1
+
+-- Display the student name and the course name.
+
+-- Hint: You need all three tables.
+
+-- Question 2
+
+-- Display the student name, course name, and grade.
+
+-- Question 3
+
+-- Display the student name, city, and course name.
+
+-- 🟡 Level 2
+-- Question 4
+
+-- Find students who are taking SQL and display their name and grade.
+
+-- Hint: You'll need:
+
+-- WHERE c.course_name = 'SQL'
+-- Question 5
+
+-- Find students with a grade greater than 85 and display:
+
+-- student name
+-- course name
+-- grade
+-- Question 6
+
+-- Display the student name, course name, and instructor for every enrollment.
+
+-- 🟠 Level 3
+-- Question 7
+
+-- Find students from Chicago or Seattle and display:
+
+-- student name
+-- city
+-- course name
+-- grade
+
+-- Hint: You can use IN.
+
+-- Question 8
+
+-- Find students who scored 80 or higher in their courses and sort them from highest grade to lowest.
+
+-- Question 9
+
+-- Find all courses taken by Alice and display:
+
+-- course name
+-- instructor
+-- grade
+-- 🔴 Challenge
+-- Question 10
+
+-- Find the students who scored 85 or higher in SQL.
+
+-- Display:
+
+-- student name
+-- course name
+-- instructor
+-- grade
