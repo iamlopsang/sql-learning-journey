00b81b352efca4572a52c73d409354014f19c6f2
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-09-08 09:02:22 +0545

    feat: add SQL script for RIGHT JOIN practice to display employees and their corresponding buildings

---
+++ left-and-right-join.sql/left-and-right-join-part2.sql
@@ -0,0 +1,12 @@
+================RIGHT JOIN PRACTICE====================
+
+    -- 1. Display all employees and the building they work in.
+
+    SELECT b.building_name, e.name
+    FROM buildings AS b
+    RIGHT JOIN employees AS e
+    ON b.building_name = e.building;
+
+-- Explanation: Use RIGHT JOIN to keep every employee and match their building information.
+
+    
