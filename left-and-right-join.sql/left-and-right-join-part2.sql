a38d5b743bfdb18763737f595186aa6bf6894446
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-09-10 12:16:40 +0545

    feat: add SQL query to find buildings with employees who have over 5 years of employment

--- left-and-right-join.sql/left-and-right-join-part2.sql
+++ left-and-right-join.sql/left-and-right-join-part2.sql
@@ -162,3 +162,13 @@
     AND e.role != 'Engineer';
 
 -- Explanation: Find non-engineers working in buildings with capacity over 20.
+
+    -- 14. Find all buildings that have at least one employee who has worked for more than 5 years.
+
+    SELECT DISTINCT e.building, e.name, e.years_employed
+    FROM buildings AS b
+    INNER JOIN employees AS e
+    ON b.building_name = e.building
+    WHERE e.years_employed > 5;
+
+-- Explanation: Join buildings with employees and keep employees with more than 5 years.
