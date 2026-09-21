60511a0e82b56628b095b1289a9e69bf93c3110a
iamlopsang <lopsang.lama.bca076@kathford.edu.np>
2026-09-21 22:11:01 +0545

    feat: add query to find employees reporting directly to Alice in self join example

--- full-join-and-self-join-sql/full_and_self_join.sql
+++ full-join-and-self-join-sql/full_and_self_join.sql
@@ -11,3 +11,16 @@ WHERE e.manager_id = m.employee_id;
 -- Explanation: Use the employees table twice.
 -- e represents the employee, and m represents the manager.
 -- Match the employee's manager_id with the manager's employee_id.
+
+    -- 2. Find all employees who report directly to Alice.
+
+    SELECT e.name AS employee_name,
+    m.name AS manager_name
+    FROM employees AS e, employees AS m
+    WHERE e.manager_id = m.employee_id
+    AND m.name = 'Alice';
+
+-- Explanation: Use the employees table twice.
+-- e represents the employee, and m represents the manager.
+-- Match the employee's manager_id with the manager's employee_id.
+-- Filter the manager's name to find employees who report directly to Alice.
